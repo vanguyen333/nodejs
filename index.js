5ddddb7d7d4bb646c9ceb172f3fb0e93b67b183f
vanguyen333 <vanguyen333@yahoo.com>
2023-08-15 14:08:36 -0400

    route set up correctly 18:29

--- index.js
+++ index.js
@@ -12,15 +12,19 @@
 // );
 
 import express, { json } from "express";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(json()); // Built-in middleware for parsing JSON
+
+app.use("/users", usersRoutes);
+
 app.get("/", (req, res) => {
-  console.log("[TEST");
   res.send("Hello from Homepage.");
 });
+
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
