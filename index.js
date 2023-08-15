21ccdd0ea27bdecf1a04c139fc47b938de73d130
vanguyen333 <vanguyen333@yahoo.com>
2023-08-15 13:43:40 -0400

    project run

---
+++ index.js
@@ -0,0 +1,23 @@
+// import express from "express";
+
+// import bodyParser from "body-parser"; //take in incomming post request body
+
+// const app = express();
+// const PORT = 5000;
+
+// app.use(bodyParser.json());
+
+// app.listen(PORT, () =>
+//   console.log(`Server running on port: http://localhost:${PORT}`)
+// );
+
+import express, { json } from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use(json()); // Built-in middleware for parsing JSON
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
