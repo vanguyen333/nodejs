e5c858251b6fcd4bcee55f08c438e2e0b19c2544
vanguyen333 <vanguyen333@yahoo.com>
2023-08-15 14:29:55 -0400

    23:52, users info print out on browser and postman

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,26 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastname: "Doe",
+    age: 24,
+  },
+];
+
 //all routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("Hello");
+  //   console.log(users);
+  //   res.send("Hello");
+  res.json(users); //send the users array as a JSON response
 });
+
+router.post("/", (req, res) => {});
 
 export default router;
