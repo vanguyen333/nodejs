8575549811bf1489328b0b15bccca03e53eca53c
vanguyen333 <vanguyen333@yahoo.com>
2023-08-15 22:05:22 -0400

    delete request works 44:00

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 //all routes in here are starting with /users
 router.get("/", (req, res) => {
@@ -23,8 +23,18 @@ router.post("/", (req, res) => {
 
 // /users/2 => req.params (id: 2)
 router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send(req.params);
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  //id to delete 123
+  //John 123
+  //Jane 321
+  users = users.filter((user) => user.id !== id);
+  res.send("User with the id ${id} deleted from the database");
 });
 
 export default router;
