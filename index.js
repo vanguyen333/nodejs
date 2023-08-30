9841fa4c36e5ab0ff44f8d5c6f647b94a82a7dc9
vanguyen333 <vanguyen333@yahoo.com>
2023-08-30 11:50:07 -0400

    added h1 home on homepage

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  if (req.url === "/") {
+    res.end("<h1>Home</h1>");
+  }
 });
 
 const PORT = process.env.PORT || 5000;
