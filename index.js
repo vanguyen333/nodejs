9d203a3c1d268075065addb2a38e5ec37e495db5
vanguyen333 <vanguyen333@yahoo.com>
2023-08-30 11:57:03 -0400

    1:06, status 200

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>Home</h1>");
   }
 });
