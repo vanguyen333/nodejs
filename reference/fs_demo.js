e30e1b877040bf76b054c0eee19c735e74d8daba
vanguyen333 <vanguyen333@yahoo.com>
2023-08-29 22:38:27 -0400

    override file 43: 35

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,4 +2,25 @@ const fs = require("fs");
 const path = require("path");
 
 //create folder
-fs.mkdir();
+// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+//   if (err) throw err;
+//   console.log("Folder created...");
+// });
+
+//create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World!",
+  (err) => {
+    if (err) throw err;
+    console.log("File Written to...");
+  }
+);
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "I love Node.js!",
+  (err) => {
+    if (err) throw err;
+    console.log("File Written to...");
+  }
+);
