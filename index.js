5fb003e6b54497eb5c393a36426b2a20206a33ac
vanguyen333 <vanguyen333@yahoo.com>
2023-08-29 21:27:12 -0400

    using class, constructor to create person to print out

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const person = require("./person");
-console.log(person);
-console.log(person.name);
+const Person = require("./person");
+
+const person1 = new Person("John Doe", 30);
+person1.greeting();
--- person.js
+++ person.js
@@ -1,6 +1,11 @@
-const person = {
-  name: "John Doe",
-  age: 30,
-};
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greeting() {
+    console.log(`My name is ${this.name}, and I am ${this.age}`);
+  }
+}
 
-module.exports = person;
+module.exports = Person;
