4336548ca67d9f310bc4042a8ef2f1b495373c8e
vanguyen333 <vanguyen333@yahoo.com>
2023-09-01 22:17:37 -0400

    1:15:20

--- index.js
+++ index.js
@@ -3,31 +3,40 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
-  }
+  // if (req.url === "/") {
+  //   fs.readFile(
+  //     path.join(__dirname, "public", "index.html"),
+  //     (err, content) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { "Content-Type": "text/html" });
+  //       res.end(content);
+  //     }
+  //   );
+  // }
+  // if (req.url === "/api/users") {
+  //   const users = [
+  //     {
+  //       name: "Bob Smith",
+  //       age: 40,
+  //     },
+  //     {
+  //       name: "John Doe",
+  //       age: 30,
+  //     },
+  //   ];
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify(users));
+  // }
 
-  if (req.url === "/api/users") {
-    const users = [
-      {
-        name: "Bob Smith",
-        age: 40,
-      },
-      {
-        name: "John Doe",
-        age: 30,
-      },
-    ];
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  }
+  //BUILD FILE PATH
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  console.log(filePath);
+
+  res.end();
 });
 
 const PORT = process.env.PORT || 5000;
