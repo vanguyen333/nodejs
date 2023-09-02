9d4daa68975a107e91e6170bf9df754330181a79
vanguyen333 <vanguyen333@yahoo.com>
2023-09-01 22:12:29 -0400

    1:12:12 http://localhost:5000/api/users

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/about") {
+  if (req.url === "/") {
     fs.readFile(
-      path.join(__dirname, "public", "about.html"),
+      path.join(__dirname, "public", "index.html"),
       (err, content) => {
         if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(content);
       }
     );
+  }
+
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob Smith",
+        age: 40,
+      },
+      {
+        name: "John Doe",
+        age: 30,
+      },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
   }
 });
 
