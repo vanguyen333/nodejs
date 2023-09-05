3c98e9d586045cc9392cca3613ac72905f0fb0ba
vanguyen333 <vanguyen333@yahoo.com>
2023-09-05 18:46:48 -0400

    #14 hello world with Express

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const http = require("http"); // node.js has been installed
-
-const hostname = "127.0.0.1"; //===localhost
-const port = 3000;
-
-//create server website
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n vandev fullstack");
+const express = require("express");
+const app = express();
+const port = 8081;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
