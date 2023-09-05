216afead13be6f04d126a1a5da13b9b63bd50420
vanguyen333 <vanguyen333@yahoo.com>
2023-09-05 19:12:49 -0400

    #17 more routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
-const app = express();
-const port = 8081;
+const express = require("express"); //import express, commonjs
+// import express form 'express'; //es modules
+
+const app = express(); //create express application
+const port = 8081; // init port
+
+//khai bao routes
+//req and res are 2 objects in Nodejs environment
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/abc", (req, res) => {
+  res.send("Chec ABC");
+});
+
+app.get("/vanguyendev", (req, res) => {
+  res.send("<h1>vanguyendev</h1>");
 });
 
 app.listen(port, () => {
