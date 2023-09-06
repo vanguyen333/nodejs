cf94787583bc85be664de78a426ab56fe8a41a4c
vanguyen333 <vanguyen333@yahoo.com>
2023-09-05 22:06:48 -0400

    #18 Template view Engine

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express"); //import express, commonjs
 // import express form 'express'; //es modules
+const path = require("path"); //common js
 
 const app = express(); //create express application
-const port = 8081; // init port
+const port = 8081; // init port ==> hardcode
+
+//config template engine
+app.set("views", path.join(__dirname, "views/"));
+app.set("view engine", "ejs");
 
 //khai bao routes
 //req and res are 2 objects in Nodejs environment
@@ -15,7 +20,8 @@ app.get("/abc", (req, res) => {
 });
 
 app.get("/vanguyendev", (req, res) => {
-  res.send("<h1>vanguyendev</h1>");
+  // res.send("<h1>vanguyendev</h1>");
+  res.render("sample.ejs");
 });
 
 app.listen(port, () => {
