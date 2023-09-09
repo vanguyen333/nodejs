3ef6db220428291c0670e44befe402a5d68600b2
vanguyen333 <vanguyen333@yahoo.com>
2023-09-08 22:14:38 -0400

    #32 Create new connection?

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
+const connection = require("../config/database");
+const { use } = require("../routes/web");
+
 const getHomepage = (req, res) => {
-  //process data
-  //call model
-  res.send("Hello World with vanguyendev and nodemon");
+  let users = [];
+
+  connection.query("SELECT * from Users u", function (err, results, fields) {
+    users = results;
+    console.log(">>>results home page=", results); // results contains rows returned by server
+
+    // console.log(">>Check users:", users);
+    res.send(JSON.stringify(users));
+  });
 };
 
 const getABC = (req, res) => {
